57110ce4241b4100e70fdba01b9cf5985cb0df14
Qurati <159145788+Qurati@users.noreply.github.com>
2025-10-25 00:01:51 +0300

    убрал окно с вебки

--- main.py
+++ main.py
@@ -6,15 +6,12 @@ import numpy as np
 cap = cv2.VideoCapture(0)
 detector = FER(mtcnn=True)
 
-
-# Функция для загрузки и изменения размера изображений
 def load_and_resize_image(image_path, target_width=640, target_height=480):
     image = cv2.imread(image_path)
     if image is None:
         print(f"Ошибка: Не удалось загрузить изображение: {image_path}")
         return np.zeros((target_height, target_width, 3), dtype=np.uint8)
 
-    # Изменяем размер изображения до целевого
     resized_image = cv2.resize(image, (target_width, target_height))
     return resized_image
 
@@ -46,18 +43,12 @@ try:
                 emotion_detected = False
 
                 for face in result:
-                    box = face["box"]
                     emotions = face["emotions"]
-
-                    x, y, w, h = box
-                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
                     emotion_type = max(emotions, key=emotions.get)
                     emotion_score = emotions[emotion_type]
 
                     emotion_text = f"{emotion_type}: {emotion_score:.2f}"
-                    cv2.putText(frame, emotion_text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
-
                     if emotion_type in emotion_images:
                         current_emotion = emotion_type
                     emotion_detected = True
@@ -71,9 +62,6 @@ try:
                 cam.send(emotion_frame_rgb)
                 cam.sleep_until_next_frame()
 
-                cv2.imshow('Emotion Detection', frame)
-                print(f"Current emotion: {current_emotion}")
-
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
 
@@ -82,4 +70,3 @@ try:
 
 finally:
     cap.release()
-    cv2.destroyAllWindows()
